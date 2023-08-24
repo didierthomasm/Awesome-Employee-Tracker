da4ac68495156493b6cb101f7347ad459cf19cef
didierthomasm <didierthomas.m@gmail.com>
2023-08-24 03:23:56 -0600

    created of schema.sql and installed dotenv

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const mysql = require('mysql2');
-require('dotenv').config();
+/*require('dotenv').config();
 
 const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
+const dbPassword = process.env.DB_PASSWORD;*/
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded( { extended:false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'nEruda44##',
+    database: 'workforce_management'
+  },
+  // This console.log should be a callback function inside createConnection
+  (err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+    } else {
+      console.log('Connected to the database.');
+    }
+  }
+);
 
-)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
